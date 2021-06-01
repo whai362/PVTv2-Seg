44639867b30fa9e76a51d41cbc303d56ccac8bad
whai362 <wangwenhai@163.com>
2021-06-01 17:27:49 +0800

    update

--- configs/sem_fpn/PVTv2/fpn_pvtv2_B1_ade20k_40k.py
+++
@@ -1,9 +0,0 @@
-_base_ = './fpn_pvtv2_B2_ade20k_40k.py'
-
-# model settings
-model = dict(
-    pretrained='pretrained/pvt_v2_b1.pth',
-    backbone=dict(
-        type='pvt_v2_b1',
-        style='pytorch'),
-    neck=dict(in_channels=[64, 128, 320, 512]))
