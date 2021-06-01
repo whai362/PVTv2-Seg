62923224339c653424af8c3b0da7ffac71f99c2e
xieenze <Johnny_ez@163.com>
2021-06-01 14:32:39 +0800

    update

--- pvtv2.py
+++ pvtv2.py
@@ -386,6 +386,24 @@ def _conv_filter(state_dict, patch_size=16):
 
 
 @BACKBONES.register_module()
+class pvt_v2_b0(PyramidVisionTransformerImpr):
+    def __init__(self, **kwargs):
+        super(pvt_v2_b0, self).__init__(
+            patch_size=4, embed_dims=[32, 64, 160, 256], num_heads=[1, 2, 5, 8], mlp_ratios=[8, 8, 4, 4],
+            qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), depths=[2, 2, 2, 2], sr_ratios=[8, 4, 2, 1],
+            drop_rate=0.0, drop_path_rate=0.1)
+
+
+
+@BACKBONES.register_module()
+class pvt_v2_b1(PyramidVisionTransformerImpr):
+    def __init__(self, **kwargs):
+        super(pvt_v2_b1, self).__init__(
+            patch_size=4, embed_dims=[64, 128, 320, 512], num_heads=[1, 2, 5, 8], mlp_ratios=[8, 8, 4, 4],
+            qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), depths=[2, 2, 2, 2], sr_ratios=[8, 4, 2, 1],
+            drop_rate=0.0, drop_path_rate=0.1)
+
+@BACKBONES.register_module()
 class pvt_v2_b2(PyramidVisionTransformerImpr):
     def __init__(self, **kwargs):
         super(pvt_v2_b2, self).__init__(
